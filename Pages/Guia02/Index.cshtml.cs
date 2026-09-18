01d184c0450a511f53c1d473a232955fead1970f
Julieta Landra <landrajulieta2@gmail.com>
2026-09-17 22:54:22 -0300

    mejoras en index principal de inicio, y agregue carpetas y archivos para la guia 2

---
+++ Pages/Guia02/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PracticasLDP3.Pages.Guia02
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
